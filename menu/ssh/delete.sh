08fdc4bebaf884e5500441da5e48ddc550610c7a
0xlvnk <kalvinaditya205@gmail.com>
2025-06-17 20:06:42 +0700

    Delete menu directory

--- menu/ssh/delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-read -p "Username yang akan dihapus: " user
-userdel -f $user && echo "Akun $user berhasil dihapus."
--- menu/ssh/list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo -e "\n=== List User SSH ===\n"
-cut -d: -f1 /etc/passwd | grep -vE '^(root|sync|shutdown|halt)' | while read user; do
-    exp=$(chage -l $user | grep "Account expires" | awk -F": " '{print $2}')
-    printf "%-15s Exp: %s\n" "$user" "$exp"
-done
-echo ""
